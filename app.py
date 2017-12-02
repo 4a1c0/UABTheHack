2c54a42d7164e097c383dabcd481340173e508e5
Pau Casacuberta Orta <pauapplecat@me.com>
2017-12-02 20:07:53 +0100

    data

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ model = appclar.models.get('food-items-v1.0')
 
 def IsPizza(url):
 
-    app = ClarifaiApp(api_key='f53aa080f78d418fb75051b03f80d3c4')
-
-    model = app.models.get('food-items-v1.0')
-    # image = ClImage(url='https://s3.amazonaws.com/clarifai-api/img2/prod/small/b8ea54acf15c4642a5ba985bda8b9633/de65940cc1524bf3a01563a7bf8ddac7')
     image = ClImage(url)
     response = model.predict([image])
     elements = response['outputs'][0]['data']['concepts']
