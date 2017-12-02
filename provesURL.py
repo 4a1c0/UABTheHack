7635f4ee1a8dc5325ca9e9e8aff1b9e88c8470a4
AdriaFiguerolaTorrell <30824745+AdriaFiguerolaTorrell@users.noreply.github.com>
2017-12-02 16:05:01 +0100

    Add files via upload

---
+++ provesURL.py
@@ -0,0 +1,31 @@
+
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+
+def IsPizza(url):
+
+	app = ClarifaiApp(api_key='f53aa080f78d418fb75051b03f80d3c4')
+
+	model = app.models.get('food-items-v1.0')
+	# image = ClImage(url='https://s3.amazonaws.com/clarifai-api/img2/prod/small/b8ea54acf15c4642a5ba985bda8b9633/de65940cc1524bf3a01563a7bf8ddac7')
+	image = ClImage(url)
+	response = model.predict([image])
+	elements = response['outputs'][0]['data']['concepts']
+
+	isPizzaThreshold = 0.85
+	isPineappleThreshold = 0.3
+	isPizza = False
+	isPineapple = False
+
+	for element in elements:
+	    if (element['name'] == 'pizza') and (element['value'] > isPizzaThreshold):
+	        isPizza = True
+	    if (element['name'] == 'pineapple') and (element['value'] > isPineappleThreshold):
+	        isPineapple = True
+
+	if isPineapple and isPizza:
+	    print('Get the hell out of here')
+	elif isPizza:
+	    print('Your pizza passes the standards of the anti pineapple pizza community.')
+
+IsPizza('https://estaticos.miarevista.es/media/cache/680x_thumb/uploads/images/article/5941496f5bafe808143c9869/ppal-pizzapina_0.jpg')
